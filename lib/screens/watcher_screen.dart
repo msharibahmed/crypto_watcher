f6e10740ae8a82d864da1f0a1f64307dbd8279ce
Sharib <msharibahmed@gmail.com>
2021-10-29 18:49:54 +0530

    add: watcher screen

---
+++ lib/screens/watcher_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WatcherScreen extends StatelessWidget {
+  static const String routeName = '/watcher';
+  const WatcherScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/utils/routing.dart
+++ lib/utils/routing.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/screens/splash_screen.dart';
 import 'package:flutter_application_2/screens/home_screen.dart';
 import 'package:flutter_application_2/screens/login_screen.dart';
+import 'package:flutter_application_2/screens/watcher_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routingMap = {
   SplashScreen.routeName: (_) => const SplashScreen(),
   LoginScreen.routeName: (_) => const LoginScreen(),
-  HomeScreen.routeName: (_) => const HomeScreen()
+  HomeScreen.routeName: (_) => const HomeScreen(),
+  WatcherScreen.routeName: (_) => const WatcherScreen()
 };
