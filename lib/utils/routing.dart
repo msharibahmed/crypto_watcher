38cc3c05399da22006c9361eefe5f8d51004a5a8
Sharib <msharibahmed@gmail.com>
2021-10-29 18:33:15 +0530

    setup routing

---
+++ lib/utils/routing.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+//
+import 'package:flutter_application_2/screens/splash_screen.dart';
+import 'package:flutter_application_2/screens/home_screen.dart';
+import 'package:flutter_application_2/screens/login_screen.dart';
+
+Map<String, Widget Function(BuildContext)> routingMap = {
+  SplashScreen.routeName: (_) => const SplashScreen(),
+  LoginScreen.routeName: (_) => const LoginScreen(),
+  HomeScreen.routeName: (_) => const HomeScreen()
+};
