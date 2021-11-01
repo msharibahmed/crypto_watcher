eb39db45c15cdd4de867c990d85a3afa58088d39
Sharib <msharibahmed@gmail.com>
2021-11-01 23:56:19 +0530

    changes: login screen color & minor

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,63 +13,47 @@ class LoginScreen extends StatelessWidget {
     final _mq = MediaQuery.of(context).size;
 
     return Scaffold(
-        backgroundColor: Colors.white,
         body: SingleChildScrollView(
-          child: Padding(
-              padding: EdgeInsets.only(
-                left: _mq.width * 0.070,
-                right: _mq.width * 0.10,
-              ),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(height: 150),
-                    _iconAndName(),
-                    const SizedBox(height: 5),
-                    FutureBuilder(
-                        future: Authentication.initializeFirebase(context),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasError) {
-                            return const Text('Error initializing Firebase');
-                          } else if (snapshot.connectionState ==
-                              ConnectionState.done) {
-                            return const GoogleSignInButton();
-                          }
-                          return const CircularProgressIndicator(
-                            color: Colors.orange,
-                          );
-                        })
-                  ])),
-        ));
+      child: Padding(
+          padding: EdgeInsets.only(
+            left: _mq.width * 0.070,
+            right: _mq.width * 0.10,
+          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 150),
+                _iconAndName(),
+                const SizedBox(height: 5),
+                FutureBuilder(
+                    future: Authentication.initializeFirebase(context),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Error initializing Firebase');
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        return const GoogleSignInButton();
+                      }
+                      return const CircularProgressIndicator(
+                        color: Colors.orange,
+                      );
+                    })
+              ])),
+    ));
   }
 
   Widget _iconAndName() => Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/gifs/login_icon.gif',
-                width: 30,
-                height: 30,
-              ),
-              const SizedBox(
-                width: 5,
-              ),
-              const Text(
-                'Crypto \nWatcher',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-              )
-            ],
+        children: const [
+          Text(
+            'Crypto \nWatcher',
+            style: TextStyle(
+                color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 30),
           ),
-          const SizedBox(
+          SizedBox(
             height: 40,
           ),
-          const Text(
+          Text(
             'Get Started',
             style: TextStyle(fontSize: 32, fontWeight: FontWeight.w100),
           ),
