b5ed5263ce8fe6e1692e0e82c9524eb93431ada7
Sharib <msharibahmed@gmail.com>
2021-11-01 18:40:44 +0530

    add: watcher model

---
+++ lib/models/watcher_model.dart
@@ -0,0 +1,11 @@
+class WatcherModel {
+  String id;
+  String cryptoId;
+  String name;
+  String priceUsd;
+  WatcherModel(
+      {required this.id,
+      required this.cryptoId,
+      required this.name,
+      required this.priceUsd});
+}
